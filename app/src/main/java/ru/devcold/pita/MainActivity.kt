eaebe3f9b91f107fbf059354f5b3905ad15e4ba6
irina <badertdinova289@gmail.com>
2021-06-30 13:34:13 +0300

    setup Firebase Crashlytics

--- app/src/main/java/ru/devcold/pita/MainActivity.kt
+++ app/src/main/java/ru/devcold/pita/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.devcold.pita
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ru.devcold.pita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }

2d6003eb9cfffcc6f2f3d61d4bd7f99ebad2a22d
irina <badertdinova289@gmail.com>
2021-08-04 14:24:58 +0300

    Add listOf(Uri) in NewOrderRecycler

--- app/src/main/java/ru/devcold/pita/NewPhotoAdapter.kt
+++ app/src/main/java/ru/devcold/pita/NewPhotoAdapter.kt
@@ -7,28 +7,36 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import ru.devcold.pita.databinding.ItemOrderBinding
+import com.bumptech.glide.Glide
+import ru.devcold.pita.databinding.ItemNewPhotoBinding
 
 class NewPhotoAdapter : ListAdapter<Uri, NewPhotoAdapter.ViewHolder>(DiffUtilCallback) {
 
     companion object {
         object DiffUtilCallback : DiffUtil.ItemCallback<Uri>() {
-            override fun areItemsTheSame(oldItem: Uri, newItem: Uri) = oldItem.path == newItem.path
+            override fun areItemsTheSame(oldItem: Uri, newItem: Uri) = oldItem.toString() == newItem.toString()
 
             override fun areContentsTheSame(oldItem: Uri, newItem: Uri) = oldItem == newItem
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemNewPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = currentList[position]
+        val context = holder.itemView.context
+
+        Glide
+            .with(context)
+            .load(currentItem)
+            .into(holder.image)
     }
 
-    class ViewHolder(binding: ItemOrderBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(binding: ItemNewPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
         val image: ImageView = binding.image
+        val remove: ImageView = binding.remove
     }
 }
--- app/src/main/java/ru/devcold/pita/fragments/NewOrderFragment.kt
+++ app/src/main/java/ru/devcold/pita/fragments/NewOrderFragment.kt
@@ -1,19 +1,15 @@
 package ru.devcold.pita.fragments
 
-import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import ru.devcold.pita.BuildConfig
 import ru.devcold.pita.NewPhotoAdapter
 import ru.devcold.pita.R
 import ru.devcold.pita.databinding.FragmentNewOrderBinding
@@ -23,20 +19,37 @@ class NewOrderFragment : Fragment() {
 
     private lateinit var binding: FragmentNewOrderBinding
     private val adapter = NewPhotoAdapter()
-    private lateinit var currentPhotoPath: String
+    private lateinit var tmpUri: Uri
 
-    companion object {
-        const val REQUEST_IMAGE_CAPTURE = 1
-        private const val IMAGE_CHOOSE = 1000;
-        private const val PERMISSION_CODE = 1001;
+    private fun getNewTmpUri(): Uri {
+        val file = File(requireContext().cacheDir, "tmpFile_${System.currentTimeMillis()}")
+        file.createNewFile()
+        file.deleteOnExit()
+        return FileProvider.getUriForFile(requireContext(), "${BuildConfig.APPLICATION_ID}.provider", file)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private val takeImageResult =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+            if (isSuccess) {
+                val newList = adapter.currentList.toMutableList()
+                newList.add(tmpUri)
+                adapter.submitList(newList)
+            }
+        }
+
+    private val selectImageFromGalleryResult =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            val newList = adapter.currentList.toMutableList()
+            newList.add(it)
+            adapter.submitList(newList)
+        }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentNewOrderBinding.inflate(inflater, container, false)
 
-        requireActivity().title = resources.getString(R.string.new_announcement)
-
         binding.newPhotoRecycler.adapter = adapter
+
+        requireActivity().title = resources.getString(R.string.new_announcement)
 
         setUpViews()
 
@@ -47,62 +60,25 @@ class NewOrderFragment : Fragment() {
         binding.addPhoto.setOnClickListener {
             createPhotoAlertDialog()
         }
+        binding.add.setOnClickListener {  }
     }
 
     private fun createPhotoAlertDialog() {
-        val builder = AlertDialog.Builder(requireActivity())
-        with(builder) {
-            setTitle(R.string.add_photo)
-            setPositiveButton(R.string.take_photo) { dialog, id ->
-                takePhoto()
+        AlertDialog.Builder(requireActivity())
+            .setTitle(R.string.add_photo)
+            .setPositiveButton(R.string.take_photo) { _, _ ->
+                takeImage()
             }
-            setNegativeButton(R.string.choose_from_gallery) { dialog, id ->
-                chooseImageGallery()
+            .setNegativeButton(R.string.choose_from_gallery) { _, _ ->
+                selectImageFromGallery()
             }
-            show()
-        }
+            .show()
     }
 
-    private fun takePhoto() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(activity, "You have not camera!", Toast.LENGTH_SHORT).show()
-        }
+    private fun takeImage() {
+        tmpUri = getNewTmpUri()
+        takeImageResult.launch(tmpUri)
     }
 
-    private fun getPhotoFile(fileName: String): File {
-        val directoryStorage = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(fileName, ".jpg", directoryStorage)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            //binding.image.setImageBitmap(imageBitmap)
-        }
-    }
-
-    private fun chooseImageGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_CHOOSE)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    chooseImageGallery()
-                }else{
-                    Toast.makeText(requireActivity(), "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
+    private fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
 }
